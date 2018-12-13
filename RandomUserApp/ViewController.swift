0e218f65e3a0336c7e19e79888cb3f354ec25447
Junier Damian <onieldp@gmail.com>
2018-12-13 19:42:11 -0400

    Initial Commit

---
+++ RandomUserApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUserApp
+//
+//  Created by Junier Damian on 12/13/18.
+//  Copyright © 2018 JunierOniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
