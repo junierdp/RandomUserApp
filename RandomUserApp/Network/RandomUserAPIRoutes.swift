7b9d56f5b93c1e339dbedf8d76884ae724f42087
Junier Damian <onieldp@gmail.com>
2018-12-15 17:21:46 -0400

    Add
    Basic API routes structure and API Client using Alamofire to manage requests.

---
+++ RandomUserApp/Network/RandomUserAPIRoutes.swift
@@ -0,0 +1,55 @@
+//
+//  RandomUserAPIRoutes.swift
+//  RandomUserApp
+//
+//  Created by Junier Damian on 12/14/18.
+//  Copyright © 2018 JunierOniel. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum RandomUserAPIRoutes: URLRequestConvertible {
+    case getUsers(page: Int, limit: Int)
+    
+    // Endpoint
+    private var endpoint: String {
+        switch self {
+        case .getUsers:
+            return ""
+        }
+    }
+    
+    // Method
+    private var method: HTTPMethod {
+        switch self {
+        case .getUsers:
+            return .get
+        }
+    }
+    
+    // Parameter
+    private var parameters: Parameters {
+        switch self {
+        case .getUsers(let page, let limit):
+            return [
+                "page": page,
+                "results": limit
+            ]
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        // Request endpoint
+        let url: URL = try ("https://randomuser.me/api/" + endpoint).asURL()
+        var request = URLRequest(url: url)
+        
+        // Request method
+        request.httpMethod = method.rawValue
+        
+        // Request parameters
+        request = try URLEncoding.methodDependent.encode(request, with: parameters)
+        
+        return request
+    }
+}
