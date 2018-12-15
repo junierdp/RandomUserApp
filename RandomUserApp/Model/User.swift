c51793c9cddfcd41df4dd0e30afffcac81f0cbe2
Junier Damian <onieldp@gmail.com>
2018-12-15 17:23:47 -0400

    Add
    - User Codable model to parse User JSON response.
    - ErrorMessage Model to decode Error JSON response.

---
+++ RandomUserApp/Model/User.swift
@@ -0,0 +1,41 @@
+//
+//  User.swift
+//  RandomUserApp
+//
+//  Created by Junier Damian on 12/14/18.
+//  Copyright © 2018 JunierOniel. All rights reserved.
+//
+
+import Foundation
+
+struct UserList: Codable {
+    var list: [User]?
+    
+    enum CodingKeys: String, CodingKey {
+        case list = "results"
+    }
+}
+struct User: Codable {
+    var name: UserName?
+    var gender: String?
+    var email: String?
+    var phone: String?
+    var cell: String?
+    var picture: UserPicture?
+    
+    internal struct UserName: Codable {
+        var title: String?
+        var first: String?
+        var last: String?
+    }
+    
+    internal struct UserPicture: Codable {
+        var large: String?
+        var medium: String?
+        var thumbnail: String?
+    }
+}
+
+
+
+
